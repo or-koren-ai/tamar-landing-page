1b3c8cf8bab2c35a11025f68b9803719a2e609f2
orkorn <orkorn@gmail.com>
2025-09-25 17:47:27 +0300

    robots host fix

--- app/robots.ts
+++ app/robots.ts
@@ -1,10 +1,11 @@
-import type { MetadataRoute } from 'next'
-import { SITE } from '@/lib/site-config'
+import type { MetadataRoute } from "next";
+import { SITE } from "@/lib/site-config";
 
 export default function robots(): MetadataRoute.Robots {
+  const base = SITE.baseUrl.replace(/\/$/, "");
   return {
-    rules: [{ userAgent: '*', allow: '/' }],
-    sitemap: `${SITE.baseUrl}/sitemap.xml`,
-    host: SITE.baseUrl,
-  }
-} 
+    rules: [{ userAgent: "*", allow: "/" }],
+    sitemap: `${base}/sitemap.xml`,
+    host: new URL(base).host, // "drkoren.skin"
+  };
+}
